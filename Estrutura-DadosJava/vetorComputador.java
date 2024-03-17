a43b18884a24044a12cdb3dd4cc41b80c5c3a58c
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2024-03-17 17:19:19 -0300

    Add files via upload

---
+++ Estrutura-DadosJava/vetorComputador.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class vetorComputador {
+    private Computador[] vetComputadors = new Computador[3];
+    private static int contador = 0;
+
+    public vetorComputador() {
+
+    }
+
+    public vetorComputador(Computador[] lComputadors) {
+        this.vetComputadors = vetComputadors;
+
+    }
+
+    public void adicionarComputador(Computador computador) {
+        this.vetComputadors = novoVetComputador();
+        this.vetComputadors[contador++] = computador;
+    }
+
+    public void mostrarComputadores() {
+        for (int i = 0; i < contador; i++) {
+            System.out.println(vetComputadors[i]);
+        }
+    }
+
+    public Computador[] novoVetComputador() {
+        Computador[] novoVetComputadors = new Computador[vetComputadors.length + 2];
+        for (int cont = 0; cont < vetComputadors.length; cont++) {
+            novoVetComputadors[cont] = vetComputadors[cont];
+
+        }
+        return novoVetComputadors;
+    }
+
+    public void atualizar(long id, Computador novComputador) {
+        for (int cont = 0; cont <= vetComputadors.length; cont++) {
+            if (vetComputadors[cont].getId() == id) {
+                vetComputadors[cont] = novComputador;
+                break;
+            }
+        }
+    }
+
+    public Computador pesquisarPorId(long id) {
+        for (int cont = 0; cont <= vetComputadors.length; cont++) {
+            if (vetComputadors[cont].getId() == id) {
+                return vetComputadors[cont];
+            } else {
+                System.out.println("Nenhuma instância encontrada com esse ID");
+
+            }
+        }
+        return null;
+    }
+
+    public void ordenarOrdemAlbabetica() {
+    
+        
+    }
+}
